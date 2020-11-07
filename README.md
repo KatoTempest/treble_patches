8422ff4fbd6c15a566ec3c4f9d68c17ef781f825
Victor Bo <bvoid@yandex.ru>
2020-11-07 07:58:23 -0500

    201107 v300i
      -wip-

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ This is GSI project, patches based on the Phh's works
 apply-patches.sh to apply all patches
 	ex. usage:
 		cd /where/aosp/sources
-		bash ~/files/patches/aosp/v300g/apply-patches.sh ~/files/patches/aosp/v300g/
+		bash ~/files/patches/aosp/v300i/apply-patches.sh ~/files/patches/aosp/v300i/
 
 buildCaos.sh for starting build process from zero
 	ex. usage:
-		cd ~/files/patches/aosp/v300g
+		cd ~/files/patches/aosp/v300i
 		bash buildCaos.sh /where/aosp/sources
 
--- buildCaos.sh
+++ buildCaos.sh
@@ -11,8 +11,8 @@ fi
 
 pushd "$1"
 
-repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r8
-git clone --single-branch https://github.com/eremitein/treble_manifest_caos -b v300g .repo/local_manifests
+repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r15
+git clone --single-branch https://github.com/eremitein/treble_manifest_caos -b v300i .repo/local_manifests
 repo sync -j${para} -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune ||exit
 bash ${rund}/apply-patches.sh ${rund}
 cd device/phh/treble
