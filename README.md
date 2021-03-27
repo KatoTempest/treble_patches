8a965470d6a7cb05cb679501d5f30aad2cf63b03
Victor Bo <bvoid@yandex.ru>
2021-03-27 10:47:00 -0400

    210403 v303

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ This is GSI project, patches based on the Phh's works
 apply-patches.sh to apply all patches
 	ex. usage:
 		cd /where/aosp/sources
-		bash ~/files/patches/aosp/v302+/apply-patches.sh ~/files/patches/aosp/v302+/
+		bash ~/files/patches/aosp/v303/apply-patches.sh ~/files/patches/aosp/v303/
 
 buildCaos.sh for starting build process from zero
 	ex. usage:
-		cd ~/files/patches/aosp/v302+
+		cd ~/files/patches/aosp/v303
 		bash buildCaos.sh /where/aosp/sources
 
--- buildCaos.sh
+++ buildCaos.sh
@@ -12,7 +12,7 @@ fi
 pushd "$1"
 
 repo init -u https://android.googlesource.com/platform/manifest -b android-11.0.0_r32
-git clone --single-branch https://github.com/eremitein/treble_manifest_caos -b v302+ .repo/local_manifests
+git clone --single-branch https://github.com/eremitein/treble_manifest_caos -b v303 .repo/local_manifests
 repo sync -j${para} -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune ||exit
 bash ${rund}/apply-patches.sh ${rund}
 cd device/phh/treble
